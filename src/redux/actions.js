96adbf1b5cd3daf9c302982a75027318679bfc5d
Kouzira <94026824+Kouzira@users.noreply.github.com>
2023-04-23 09:28:34 +0700

    Add files via upload

---
+++ src/redux/actions.js
@@ -0,0 +1,153 @@
+import * as types from "./actionTypes";
+import { auth, googleAuthProvider, facebookAuthProvider } from "../firebase";
+
+export const addToBasket = (item) => ({
+  type: types.ADD_TO_BASKET,
+  payload: item,
+});
+
+export const removeFromBasket = (id) => ({
+  type: types.REMOVE_FROM_BASKET,
+  payload: id,
+});
+
+const registerStart = () => ({
+  type: types.REGISTER_START,
+});
+
+const registerSuccess = ({ user, additionalData }) => ({
+  type: types.REGISTER_SUCCESS,
+  payload: { user, additionalData },
+});
+
+const registerError = (error) => ({
+  type: types.REGISTER_FAIL,
+  payload: error,
+});
+
+const loginStart = () => ({
+  type: types.LOGIN_START,
+});
+
+const loginSuccess = (user) => ({
+  type: types.LOGIN_SUCCESS,
+  payload: user,
+});
+
+const loginError = (error) => ({
+  type: types.LOGIN_FAIL,
+  payload: error,
+});
+
+const googleSignInStart = () => ({
+  type: types.GOOGLE_SIGN_IN_START,
+});
+
+const googleSignInSuccess = (user) => ({
+  type: types.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+const googleSignInFail = (error) => ({
+  type: types.GOOGLE_SIGN_IN_FAIL,
+  payload: error,
+});
+
+const fbSignInStart = () => ({
+  type: types.FACEBOOK_SIGN_IN_START,
+});
+
+const fbSignInSuccess = (user) => ({
+  type: types.FACEBOOK_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+const fbSignInFail = (error) => ({
+  type: types.FACEBOOK_SIGN_IN_FAIL,
+  payload: error,
+});
+
+const logoutStart = () => ({
+  type: types.LOGOUT_START,
+});
+
+const logoutSuccess = () => ({
+  type: types.LOGOUT_SUCCESS,
+});
+
+const logoutError = (error) => ({
+  type: types.LOGOUT_FAIL,
+  payload: error,
+});
+
+export const setuser = (user) => ({
+  type: types.SET_USER,
+  payload: user,
+});
+
+export const setBasketEmpty = () => ({
+  type: types.SET_BASKET_EMPTY,
+});
+
+export const registerInitiate = (email, password, displayName) => {
+  return function (dispatch) {
+    dispatch(registerStart());
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        // user.displayName = displayName;
+        // dispatch(registerSuccess({ user, additionalData: { displayName } }));
+        user.updateProfile({
+          displayName,
+        });
+        dispatch(registerSuccess({ user, additionalData: { displayName } }));
+      })
+      .catch((error) => dispatch(registerError(error.message)));
+  };
+};
+
+export const loginInitiate = (email, password) => {
+  return function (dispatch) {
+    dispatch(loginStart());
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        dispatch(loginSuccess(user));
+      })
+      .catch((error) => dispatch(loginError(error.message)));
+  };
+};
+
+export const googleSignInInitiate = () => {
+  return function (dispatch) {
+    dispatch(googleSignInStart());
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(({ user }) => {
+        dispatch(googleSignInSuccess(user));
+      })
+      .catch((error) => dispatch(googleSignInFail(error.message)));
+  };
+};
+
+export const fbSignInInitiate = () => {
+  return function (dispatch) {
+    dispatch(fbSignInStart());
+    auth
+      .signInWithPopup(facebookAuthProvider.addScope("user_birthday, email"))
+      .then((result) => {
+        dispatch(fbSignInSuccess(result.user));
+      })
+      .catch((error) => dispatch(fbSignInFail(error.message)));
+  };
+};
+
+export const logOutInitiate = () => {
+  return function (dispatch) {
+    dispatch(logoutStart());
+    auth
+      .signOut()
+      .then((resp) => dispatch(logoutSuccess()))
+      .catch((error) => dispatch(logoutError(error.message)));
+  };
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+// import { persistStore } from "redux-persist";
 
+import rootReducer from "./root-reducer";
 
-export const store = configureStore({
-  reducer: {
+const middleware = [thunk];
 
-  },
-});
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
+
+// export const persistor = persistStore(store);
