89e940c4947b2422c74c4c8cef1b673be45b296b
TRAN THANH SON <21522557@gm.uit.edu.vn>
2023-05-05 22:35:13 +0700

    Adding search box and user avatar for header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,26 +1,48 @@
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import SearchBox from '../component/SearchBox';
+import Avatar from '../component/Avatar';
 
 const HeaderLayout = (props) => {
-    const { children } = props
+  const { children } = props;
 
-    return ( 
-        <Container>
-            <Header>
+  return (
+    <Container>
+      <Header>
+        <SearchBoxWrapper>
+          <SearchBox />
+        </SearchBoxWrapper>
+        <AvatarWrapper>
+          <Avatar />
+        </AvatarWrapper>
+      </Header>
+      {children}
+    </Container>
+  );
+};
 
-            </Header>
-
-            {children}
-        </Container>
-
-     );
-}
- 
 export default HeaderLayout;
 
-const Container = styled.div `
-`
+const Container = styled.div``;
 
-const Header = styled.div` 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
 
+const SearchBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-basis: 100%;
+  justify-content: center;
+  margin-left : 10rem;
+`;
 
-`
+const AvatarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
