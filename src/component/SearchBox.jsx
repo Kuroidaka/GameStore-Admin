623599d04199250c8e9c4292bcbe2f9772f7ed63
TRAN THANH SON <21522557@gm.uit.edu.vn>
2023-05-05 22:32:43 +0700

    Add SearchBox component

---
+++ src/component/SearchBox.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const SearchBox = () => {
+  return (
+    <SearchContainer>
+      <SearchInput type="text" placeholder="Search" />
+      <SearchIcon>
+        <FontAwesomeIcon icon={faSearch} />
+      </SearchIcon>
+    </SearchContainer>
+  );
+};
+
+export default SearchBox;
+
+const SearchContainer = styled.div`
+  position: relative;
+  width: 20%;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  padding: 8px 12px 8px 40px;
+  border: none;
+  border-radius: 24px;
+  background-color: #f0f0f0;
+  outline: none;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  min-width: 150px;
+`;
+
+const SearchIcon = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 12px;
+  transform: translateY(-50%);
+  font-size: 20px;
+  color: #555;
+`;
